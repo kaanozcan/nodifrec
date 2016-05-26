d65f264106c84bf3b878b945c9c9a6761b267f90
Kaan Özcan <kaan.ozcan@PT0004.local>
2016-05-27 00:46:12 +0300

    server to render fixtures

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,26 @@
 import express from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { fixtures } from "../nodifrec";
 
 export default function (config){
-  const server = express();
+  const server =  express();
 
-  server.use("/", function (req, res){
-    req.status(200).send("<h3>I'm up!</h3>");
+  server.use("/fixtures/:name", function (req, res) {
+    var fixture = fixtures.get(req.params.name),
+        markup = renderToString(fixture.render());
+
+    res.status(200).send(`
+      <html>
+        <head>
+
+        </head>
+          ${markup}
+        </body>
+      </html>`
+    );
   });
+
+  server.listen(config.port);
+
 }
