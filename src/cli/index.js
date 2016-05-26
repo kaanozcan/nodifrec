7fe78b00263136ac938e9f4641c220af1b350f33
Kaan Özcan <kaan.ozcan@PT0004.local>
2016-05-27 00:43:55 +0300

    refrence and test images saving feature

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,54 +1,83 @@
-import cli from "commander";
-import casper from "casper";
-
-import packageJson from "./package.json";
+#!/usr/bin/env node
+import phantom from "phantom";
+import cmd from "commander";
+import glob from "glob";
+import packageJson from "../../package.json";
 import defaultConfig from "../constants/defaultConfig";
-import server from "../server";
+import createServer from "../server";
+import { fixtures } from "../nodifrec";
 
-function getConfig(userConfigPath) {
-  const userConfig = require(userConfigPath);
+const userConfig = require("../../../../nodifrec.config.js"),
+      config = Object.assign({}, defaultConfig, userConfig),
+      server = createServer(config);
 
-  return Object.assign({}, defaultConfig, userConfig);
-}
-
-function init() {
-  const config = getConfig("./defaultConfig.json", "../../nodifrec.config");
-
-  return startServer(config);
-}
-
-function startServer(config) {
-  return new Promise((resolve, reject) => {
-    const session = casper.start(`http://localhost:${defaultConfig.port}`, function (){
-      if(this.status().currentHTTPStatus == 200){
-        resolve(this);
-      } else {
-        server(config);
-        startServer().then((casper) => {
-          resolve(casper);
+function executeFixtures(){
+  return new Promise(function (resolve) {
+    glob(process.cwd() + config.src, function (err, paths) {
+      if (!err) {
+        paths.forEach(function (path) {
+          require("../../../../" + config.requireHook);
+          require(path);
         });
+
+        resolve();
       }
     });
   });
 }
 
-cli
+function createPhantom() {
+  return phantom.create().then(function (instance) {
+    return instance.createPage().then((page) => Promise.resolve({instance, page}));
+  });
+}
+
+function openFixturePages(){
+  return executeFixtures()
+    .then(() => createPhantom())
+    .then(({instance, page}) => {
+      const pages = Array.from(fixtures.values()).map(fixture => {
+        const url = `http://localhost:${config.port}/fixtures/${fixture.name}`;
+        return page.open(url).then((status) => Promise.resolve({page, instance, status, fixture}));
+      });
+
+      return Promise.all(pages);
+    });
+}
+
+cmd
   .version(packageJson.version);
 
-cli
+cmd
   .command("reference")
   .description("takes reference of each fixture to be differentiated later")
-  .action(() => {
-    init().then(() => {
-      console.log("site is up");
-    });
-  });
+  .action(function () {
+    console.log("Saving reference images.");
 
-cli
+    openFixturePages()
+      .then(function (values) {
+        values.forEach(({instance, page, status, fixture}) => {
+          const path = `${process.cwd()}/nodifrec-data/reference/${fixture.name}.png`;
+          page.render(path);
+          console.log(`${path}: ${status}`);
+        });
+      });
+});
+
+cmd
   .command("test")
   .description("differentiates reference images")
-  .action(() => {
-    init().then(() => {
-      console.log("site is up");
-    });
-  });
+  .action(function () {
+    console.log("Saving test images.");
+
+    openFixturePages()
+      .then(function (values) {
+        values.forEach(({instance, page, status, fixture}) => {
+          const path = `${process.cwd()}/nodifrec-data/test/${fixture.name}.png`;
+          page.render(path);
+          console.log(`${path}: ${status}`);
+        });
+      });
+});
+
+cmd.parse(process.argv);
